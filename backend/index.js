bd52b33773d27f32bea6bdc4e7163e6cb844c1d0
rupnarayanb-asconsoft <rupnarayan@asconsoft.com>
2026-05-23 01:43:00 +0530

    Added Express server with multiple routes

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,40 @@
 const http = require('http');
 const express = require('express');
 
-console.log(express);
-const server = http.createServer(function(req,response){
-    console.log(req.url);
-    if(req.url === '/'){
-        response.end("home");
-    }else if(req.url === '/time'){
-        const date = new Date();
-        response.end(date.toLocaleString())
-    }else{
-        response.end("not homes")
-    }
+//console.log(express);
+// const server = http.createServer(function(req,response){
+//     console.log(req.url);
+//     if(req.url === '/'){
+//         response.end("home");
+//     }else if(req.url === '/time'){
+//         const date = new Date();
+//         response.end(date.toLocaleString())
+//     }else{
+//         response.end("not homes")
+//     }
     
+// });
+const app = express();
+app.get('/', (req, response)=>{
+    response.send('home')
 });
-server.listen(3000,()=>{
+
+app.get('/about', (req, response)=>{
+    response.send('about')
+});
+
+app.get('/time', (req, response)=>{
+    const date = new Date();
+    response.send(date.toLocaleString())
+});
+
+app.listen(3000,()=>{
     console.log('ccc')
-});
+    });
+
+// server.listen(3000,()=>{
+//     console.log('ccc')
+// });
 
 
 
--- backend/readme.md
+++ backend/readme.md
@@ -1,1 +1,7 @@
-#Test
+## Backend Progress
+- Migrated from raw Node HTTP server to Express
+- Added route handling
+- Implemented:
+  - /
+  - /about
+  - /time
