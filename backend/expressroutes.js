51b5a8e4258d179a0faa9072caa1be4b0839996e
rupnarayanb-asconsoft <rupnarayan@asconsoft.com>
2026-06-27 02:10:47 +0530

    db

--- backend/expressroutes.js
+++ backend/expressroutes.js
@@ -36,57 +36,118 @@ function createRouterFactory(state){
         
     })
 
-    router.put('/api/users/:id',(req,res)=>{
+    router.put('/api/users/:id',async (req,res)=>{
         const userdata = req.body;
         const userId = Number(req.params.id);
-      
-        const user = users.find((item) => item.id === userId);
-        const userIndex = users.findIndex((item) => item.id === userId);
-    
-        if (!user) {
-            return res.status(404).send({ message: 'User not found' });
+
+        if(!userdata.name || !userdata.role){
+            return res.status(400).send({
+                message:"Name or Role is missing"
+            })
         }
-    
-        const updatedUser = {
-            "id": userId,
-            "name": userdata.name ? userdata.name : user.name,
-            "role": userdata.role ? userdata.role : user.role                                  
-        };
-    
-        users[userIndex] = updatedUser;
-        console.log(users);
-    
-        return res.send({
+
+        const queryText = `UPDATE users SET name = $1, role = $2 WHERE id = $3 RETURNING *`;
+        const queryValues = [userdata.name,userdata.role, userId];
+
+
+        try{
+            const updatedUser = await db.query(queryText, queryValues);
+            return res.status(200).send({
             "message": "User updated successfully",
-            "user": updatedUser
-        })
+            "user": updatedUser.rows[0]
+            })
+
+            if(!updatedUser.rows[0]){
+                return res.status(404).send({
+                    "message":"User not found"
+                })
+            }
+        }catch(error){
+            return res.status(500).send({
+                "message":"Internal server error"
+            });
+        }
+       
+    
+    
+        
     })
 
-    router.delete('/api/users/:id',(req,res)=>{
+    router.delete('/api/users/:id',async (req,res)=>{
         const userId = Number(req.params.id);
-       
+    
+
+        const queryText = `DELETE FROM users WHERE id = $1 RETURNING *`;
+        const queryValues = [userId];
+
+        try {
+            const deletedUser = await db.query(queryText, queryValues);
+            if (!deletedUser.rows[0]) {
+                return res.status(404).send({
+                    "message": "User not found"
+                });
+            }
+            return res.status(200).send({
+                "message": "User deleted successfully",
+                "user": deletedUser.rows[0]
+            });
+        } catch (error) {
+            return res.status(500).send({
+                "message": "Internal server error"
+            });
+        }
+    });
+
+    router.get('/api/users/:id',async (req,res)=>{
+        const userId = Number(req.params.id);
         const user = users.find((item) => item.id === userId);
-        const userIndex = users.findIndex((item) => item.id === userId);
     
         if (!user) {
             return res.status(404).send({ message: 'User not found' });
         }
     
-        const deleteUser = {
-            "name": user.name,
-            "role": user.role,
-        };
-    
-    
-        users.splice(userIndex,1);
-    
-        console.log(users);
-    
-        return res.send({
-            "message": "User deleted successfully",
-            "user": deleteUser
-        })
+        return res.send(user);
     })
+
+    router.get('/', (req, response)=>{
+        response.send('home')
+    });
+
+    
+        router.get('/api/users',async (req,response)=>{
+            try{
+              const allUser = await db.query('SELECT * FROM users');
+              response.json(allUser.rows);
+            }catch(error){
+                response.status(500).json({"message":"Database Error"});
+            }
+        });
+    
+
+    
+
+    router.get('/about',(req,response)=>{
+        response.send('about asdsad')
+    });
+
+    router.get('/api/user',(req,response)=>{
+        response.send({'name':"rup", 'role':'rrlkj'})
+    });
+
+    router.get('/time',(req,response)=>{
+        const date = new Date();
+        response.send(date.toLocaleString());
+    });
+
+    router.get('/api/products',(req,res)=>{
+        res.send([{'name':"produc1", 'owner':'Rup'}, {'name':"produc2", 'owner':'Rup2'}])
+    })
+
+    router.get('/api/profile',(req,res)=>{
+        res.send([{'name':'Rup', 'skills':{'frontend':true, 'backend':true}}])
+    });
+
+    
 
     router.get('/api/users/:id',(req,res)=>{
         const userId = Number(req.params.id);
